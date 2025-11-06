acfba2686c819d1fdd36639fd60f7655265f62b1
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-11-06 07:42:58 +0100

    Create GeoJsonLayer type

--- kmp-maps/core/src/commonMain/kotlin/com/swmansion/kmpmaps/core/Map.kt
+++ kmp-maps/core/src/commonMain/kotlin/com/swmansion/kmpmaps/core/Map.kt
@@ -94,6 +94,7 @@ import androidx.compose.ui.Modifier
  * @param onMapLongClick Callback invoked when the user long-clicks on the map
  * @param onPOIClick Callback invoked when the user clicks on a Point of Interest
  * @param onMapLoaded Callback invoked when the map has finished loading
+ * @param geoJsonLayers List of GeoJSON layers to display on the map
  */
 @Composable
 public expect fun Map(
@@ -114,4 +115,5 @@ public expect fun Map(
     onMapLongClick: ((Coordinates) -> Unit)? = null,
     onPOIClick: ((Coordinates) -> Unit)? = null,
     onMapLoaded: (() -> Unit)? = null,
+    geoJsonLayers: List<GeoJsonLayer> = emptyList(),
 )
