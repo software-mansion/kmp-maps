77dfb752767cbd01a9a60f48c4ff22e984628afb
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2026-03-06 12:16:51 +0100

    Add iosX64 target (#151)

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -20,7 +20,7 @@ kotlin {
     jvmToolchain(17)
     androidTarget { compilerOptions { jvmTarget.set(JvmTarget.JVM_11) } }
 
-    listOf(iosArm64(), iosSimulatorArm64()).forEach { iosTarget ->
+    listOf(iosX64(), iosArm64(), iosSimulatorArm64()).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "Sample"
             isStatic = true
