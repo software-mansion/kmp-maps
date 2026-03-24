69f97ed0ee2b06b48ba66910878cdd971518b421
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2026-03-24 10:29:40 +0100

    Format

--- kmp-maps/google-maps/src/iosMain/kotlin/com/swmansion/kmpmaps/googlemaps/MapDelegate.kt
+++ kmp-maps/google-maps/src/iosMain/kotlin/com/swmansion/kmpmaps/googlemaps/MapDelegate.kt
@@ -15,11 +15,11 @@ import cocoapods.Google_Maps_iOS_Utils.GMUClusterManager
 import com.swmansion.kmpmaps.core.CameraPosition
 import com.swmansion.kmpmaps.core.Circle
 import com.swmansion.kmpmaps.core.Coordinates
+import com.swmansion.kmpmaps.core.IosCameraPosition
 import com.swmansion.kmpmaps.core.Marker
 import com.swmansion.kmpmaps.core.Polygon
 import com.swmansion.kmpmaps.core.Polyline
 import com.swmansion.kmpmaps.core.getId
-import com.swmansion.kmpmaps.core.IosCameraPosition
 import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.ObjCSignatureOverride
@@ -64,10 +64,11 @@ internal class MapDelegate(
                         coordinates = Coordinates(latitude = latitude, longitude = longitude),
                         zoom = didChangeCameraPosition.zoom(),
                         bounds = visibleBounds,
-                        iosCameraPosition = IosCameraPosition(
-                            gmsBearing = didChangeCameraPosition.bearing().toFloat(),
-                            gmsViewingAngle = didChangeCameraPosition.viewingAngle().toFloat()
-                        )
+                        iosCameraPosition =
+                            IosCameraPosition(
+                                gmsBearing = didChangeCameraPosition.bearing().toFloat(),
+                                gmsViewingAngle = didChangeCameraPosition.viewingAngle().toFloat(),
+                            ),
                     )
                 it(cameraPosition)
             }
